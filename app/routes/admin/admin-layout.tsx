937571245c59e190010e977f11f40cf48af36a08
sherlock-18 <143173719+sherlock-18@users.noreply.github.com>
2025-06-18 23:03:18 +0530

    routing

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,8 +1,14 @@
-import React from "react";
+import {Outlet} from "react-router";
 const AdminLayout = () => {
     
     return (
-        <div className="admin-layout">AdminLayout</div>
+        <div className="admin-layout">
+        Mobile sidebar
+        <aside className="w-full max-w-[270px] hidden lg:block">sidebar</aside>
+        <aside className="children">
+            <Outlet/>
+        </aside>
+        </div>
     )
 }
 
---
+++ app/routes/admin/all-users.tsx
@@ -0,0 +1,8 @@
+const AllUsers = () => {
+    
+    return(
+        <div>Users Table</div>
+    ) 
+}
+
+export default AllUsers;
